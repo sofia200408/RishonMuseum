d24172ff063aebf732fcf76ff6c6ca8aa17b00bc
adiAlicecode <adiAlicecode@gmail.com>
2020-03-15 14:34:44 +0200

    add style

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Link} from "react-router-dom";
 import './home.css'
+import logo from '../img/girl2.png';
+import bubble from '../img/bubble-text.png'
 
 class Home extends Component{
     data;
@@ -26,29 +28,35 @@ class Home extends Component{
 
     render(){
         return(
-            <form>
-                <label>
-                    User Name:
-                    <input type="text" onChange={this.handleChange} required/>
-                </label>
-            <div>
-                <label>Number of Users: 
-                    <input type="number" required/>
-                </label>
-                <label>City:
-                    <input type="text" required/>
-                </label>
-             </div>
-             <div>
-             <label>Team Number:
-                    <input type="number" required/>
-                </label>
-             </div>
-             <Link to="/codescreen">
-                <button onClick={this.handleFormSubmit}>Click</button>
-            </Link>
-            </form>
-           
+        <form>
+            <img id="img2" src={logo} alt="Logo" />
+            <img id="bubble" src={bubble} alt="Logo" />
+            <div id="text"> <h3>Hello, you are in <br/>"Search for Alice" game! <br /> Fill in the deateils <br />and start to play!</h3>
+            </div>
+            <hr />
+            <div className="box">
+                    <label>
+                        User Name:
+                        <input type="text" onChange={this.handleChange} required/>
+                    </label>
+                <div>
+                    <label>Number of Users: 
+                        <input type="number" required/>
+                    </label>
+                    <label>City:
+                        <input type="text" required/>
+                    </label>
+                </div>
+                <div>
+                <label>Team Number:
+                        <input type="number" required/>
+                    </label>
+                </div>
+                <Link to="/codescreen">
+                    <button onClick={this.handleFormSubmit}>Click</button>
+                </Link>
+            </div>
+        </form>
         )
     }
 }
